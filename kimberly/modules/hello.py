694b4f66f53d216d16666cfb770d4aea1144694a
Sonico98 <lol6028@gmail.com>
2023-01-04 13:22:36 -0300

    Hello module only in groups

--- kimberly/modules/hello.py
+++ kimberly/modules/hello.py
@@ -13,13 +13,13 @@ def build_reply(message, answer):
     return answer
 
 
-@kimberly.on_message(filters.regex(regex_salute))
+@kimberly.on_message(filters.group & filters.regex(regex_salute))
 @kimberly.on_edited_message(filters.regex(regex_salute))
 async def hello(client, message):
     answer = build_reply(message, salute_answers)
     await message.reply_text(answer, quote=False)
 
-@kimberly.on_message(filters.regex(regex_gn))
+@kimberly.on_message(filters.group & filters.regex(regex_gn))
 @kimberly.on_edited_message(filters.regex(regex_gn))
 async def night(client, message):
     answer = build_reply(message, gn_answers)
