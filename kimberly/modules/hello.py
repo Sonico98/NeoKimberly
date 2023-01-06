9c7709ba39dca2b0fe722a25e3b3bad754b67178
Sonico98 <lol6028@gmail.com>
2023-01-06 11:23:51 -0300

    Fix regex to match only whole words, and fix reply function
    Wrap every regex pattern between "\\b" so that "ola" is matched but
    "piola" is not.
    Also I forgot to get the answer in the build_reply function from the passed parameters.

--- kimberly/modules/hello.py
+++ kimberly/modules/hello.py
@@ -2,14 +2,15 @@ from pyrogram import filters
 from neokimberly import kimberly
 import random
 
-regex_salute = "hola|holi(s)?|aló|ola|(bu|w)enas|buenos\\sd(i|í)as"
-regex_gn = "buenas noches|hasta mañana|gn|oyasumi|konbanwa|good night"
-salute_answers = ['Hola ', 'Hola a todos, menos a ']
+regex_salute = "\\bhola\\b|\\bholi(s)?\\b|\\baló\\b|\\bola\\b|(bu|w)enas|\\bbuenos\\sd(i|í)as\\b"
+regex_gn = "\\bbuenas noches\\b|\\bhasta mañana\\b|\\bgn\\b|\\boyasumi\\b|\\bkonbanwa\\b|\\bgood night\\b"
+salute_answers = ['Hola ', 'Hola a todos, menos a ', 'Saludos, ']
 gn_answers = ['Buenas noches ', 'Tengan todos una buena noche, excepto ']
+
 
 def build_reply(message, answer):
     user = message.from_user.first_name
-    answer = random.choice(salute_answers) + user
+    answer = random.choice(answer) + user
     return answer
 
 
@@ -18,6 +19,7 @@ def build_reply(message, answer):
 async def hello(client, message):
     answer = build_reply(message, salute_answers)
     await message.reply_text(answer, quote=False)
+
 
 @kimberly.on_message(filters.group & filters.regex(regex_gn))
 @kimberly.on_edited_message(filters.group & filters.regex(regex_gn))
