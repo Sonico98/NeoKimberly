fd7e80ab947f25064f736ec9fb5a180319890f6b
Sonico98 <lol6028@gmail.com>
2023-01-06 11:22:55 -0300

    Wrap functions inside a main function

--- kimberly/__main__.py
+++ kimberly/__main__.py
@@ -3,17 +3,21 @@ from db.mongodb import db_client
 from modules import *
 from pyrogram.sync import idle
 import asyncio
+import sys
+
 
 async def get_server_info():
     print("Trying to connect to MongoDB...")
     try:
         await db_client.server_info()
         print("Success!\n")
+        return True
     except Exception:
         print("\nUnable to connect to the database.")
         print("Make sure the Mongo DataBase is running and " +
               "configured correctly")
-        exit(2)
+        return False
+
 
 async def start_pyrogram():
     print("Starting the bot...")
@@ -30,7 +34,14 @@ async def start_pyrogram():
         exit(2)
 
 
+async def main():
+    db_is_running = await get_server_info()
+    if (db_is_running):
+        await start_pyrogram()
+        sys.exit(0)
+    sys.exit(1)
+
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_server_info())
-    loop.run_until_complete(start_pyrogram())
+    loop.run_until_complete(main())
