4199c96a7a2e58061128fa59c3163032206a23d7
Oblivious-Man <121920559+Oblivious-Man@users.noreply.github.com>
2023-01-07 14:57:19 -0300

    Upgraded /trinsfirmir command
    Added the "reply to a text" message and modified the code where the vowels are replaced to make it shorter.
    Note: I had to separate the normal vowels with tilde vowels because it gives an "string out of index" error. besides that the code works very well with no errors so far.

--- kimberly/modules/trinsfirmir.py
+++ kimberly/modules/trinsfirmir.py
@@ -6,26 +6,20 @@ def trinsfirmir(message):
     vocalesmax = "AEOUÄËÖÜÂÊÔÛÀÈÒÙ"
     vocalmintil = "áéóú"
     vocalmaxtil = "ÁÉÓÚ"
-    if len(message.reply_to_message.text) == "":
-        print(len(message.reply_to_message.text))
+    if (message.reply_to_message is None):
         texto = "Respondele a algo que tenga texto."
-        # @kimberly.send_message(message.chat.id, texto)
         return texto
-    else:
-        string = (message.reply_to_message.text)
-        for x in range(len(vocalesmin)):
-            string = string.replace(vocalesmin[x],"i")
-        for x in range(len(vocalesmax)):            
-            string = string.replace(vocalesmax[x],"I")
-        for x in range(len(vocalmintil)):            
-            string = string.replace(vocalmintil[x],"í")
-        for x in range(len(vocalmaxtil)):            
-            string = string.replace(vocalmaxtil[x],"Í")
+    
+    string = (message.reply_to_message.text)
+    for x in range(len(vocalesmin)):
+        string = string.replace(vocalesmin[x],"i").replace(vocalesmax[x],"I")
+    for x in range(len(vocalmintil)):
+        string = string.replace(vocalmintil[x],"í").replace(vocalmaxtil[x],"Í")
     return string
        
 @kimberly.on_message(filters.regex("^/trinsfirmir$"))
 async def reply(client, message):
     answer = trinsfirmir(message)
-    await message.reply_text(answer, quote=False)
+    await message.reply_text(answer, quote=True)
     
     # (\)
