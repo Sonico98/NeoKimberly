4e7311b4121c6969e3172afd319b6fb9c55c552f
Sonico98 <lol6028@gmail.com>
2023-01-09 13:00:27 -0300

    Use filters.command instead of filters.regex
    Fixes the problem where sending '/trinsfirmir <text>' the bot would
    remain silent.

--- kimberly/modules/trinsfirmir.py
+++ kimberly/modules/trinsfirmir.py
@@ -17,9 +17,8 @@ def trinsfirmir(message):
         string = string.replace(vocalmintil[x],"í").replace(vocalmaxtil[x],"Í")
     return string
        
-@kimberly.on_message(filters.regex("^/trinsfirmir$"))
+@kimberly.on_message(filters.command("trinsfirmir"))
 async def reply(client, message):
     answer = trinsfirmir(message)
     await message.reply_text(answer, quote=True)
-    
-    # (\)
+
