9cb9338e5c777e1dc24abf841452592f562e45b5
Sonico98 <lol6028@gmail.com>
2023-01-16 21:31:01 -0300

    Replace `filters.regex` with `filters.command`

--- kimberly/modules/ping.py
+++ kimberly/modules/ping.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pyrogram import filters
 from neokimberly import kimberly
 
-@kimberly.on_message(filters.regex("^/ping$"))
+@kimberly.on_message(filters.command("ping"))
 async def ping_me(client, message):
     start = datetime.now()
     reply = await message.reply_text("`Pong!`")
